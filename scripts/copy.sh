78654166088f8a3f5b5d8a980cc6fd1e004a4e1c
Joshua Raphael <josh.n.raphael@gmail.com>
2025-05-10 01:01:15 -0700

    move autocr and RATools unpacking into docker build stage

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -12,9 +12,22 @@ if [ -z "${GAME_ID}" ]; then
   exit 1
 fi
 
+if [ -z "${REPORT}" ]; then
+  echo "REPORT env var is not set"
+  exit 1
+fi
+
+REPORT_STR=""
+true=("true" "TRUE" "True")
+if [[ ${true[@]} =~ ${REPORT} ]]; then
+  REPORT_STR="true"
+fi
+
 export CONTAINER_HOME=$(cat $CONTAINER_HOME_FILE)
 
 docker cp rascript-audit-img:${CONTAINER_HOME}/Installs/RALibretro-x64/RACache/Data/${GAME_ID}-Notes.json container_home/
 docker cp rascript-audit-img:${CONTAINER_HOME}/Installs/RALibretro-x64/RACache/Data/${GAME_ID}-User.txt container_home/
 docker cp rascript-audit-img:${CONTAINER_HOME}/Installs/RALibretro-x64/RACache/Data/${GAME_ID}-Rich.txt container_home/
-docker cp rascript-audit-img:${CONTAINER_HOME}/Installs/RALibretro-x64/RACache/Data/${GAME_ID}-Report.txt container_home/
+if [ ! -z "${REPORT_STR}" ]; then
+  docker cp rascript-audit-img:${CONTAINER_HOME}/Installs/RALibretro-x64/RACache/Data/${GAME_ID}-Report.txt container_home/
+fi
--- scripts/entry.sh
+++ scripts/entry.sh
@@ -29,8 +29,9 @@ if [[ ${true[@]} =~ ${REPORT} ]]; then
   REPORT_STR=" --report"
 fi
 
-export RATOOLS_DIR=${HOME}/Installs/RATools-${RATOOLS_VERSION}
-export RALIBRETRO_DIR=${HOME}/Installs/RALibretro-x64
+export INSTALLS_DIR=${HOME}/Installs
+export RATOOLS_DIR=${INSTALLS_DIR}/RATools-${RATOOLS_VERSION}
+export RALIBRETRO_DIR=${INSTALLS_DIR}/RALibretro-x64
 export RA_DATA_DIR=${RALIBRETRO_DIR}/RACache/Data
 export CODENOTES_DIR=${HOME}/rascript-audit-codenotes
 
@@ -43,19 +44,16 @@ mkdir -p /app/rascript
 mv ${APP_DIR}/go.mod ${CODENOTES_DIR}
 mv ${APP_DIR}/go.sum ${CODENOTES_DIR}
 mv ${APP_DIR}/main.go ${CODENOTES_DIR}
-mv ${APP_DIR}/autocr-cli-${AUTOCRCLI_VERSION}.zip ${HOME}
-mv ${APP_DIR}/RATools-${RATOOLS_VERSION}.zip ${HOME}
+mv ${APP_DIR}/autocr-cli-${AUTOCRCLI_VERSION}/autocr-cli-${AUTOCRCLI_VERSION} ${INSTALLS_DIR}
+mv ${APP_DIR}/RATools-${RATOOLS_VERSION} ${INSTALLS_DIR}
 touch ${RA_DATA_DIR}/${GAME_ID}.json
+rm -rf mv ${APP_DIR}/autocr-cli-${AUTOCRCLI_VERSION}
+rm -rf ${APP_DIR}/RATools-${RATOOLS_VERSION}
 
-unzip ${HOME}/autocr-cli-${AUTOCRCLI_VERSION}.zip -d ${HOME}
-rm ${HOME}/autocr-cli-${AUTOCRCLI_VERSION}.zip
-
-unzip ${HOME}/RATools-${RATOOLS_VERSION}.zip -d ${RATOOLS_DIR}
-rm ${HOME}/RATools-${RATOOLS_VERSION}.zip
 cd ${CODENOTES_DIR} && go get -t ./... && go run main.go > ${RA_DATA_DIR}/${GAME_ID}-Notes.json
 cp "/app/rascript/${RASCRIPT_FILE}" ${HOME}/${GAME_ID}.rascript
 echo ${HOME} > /app/home.txt
 wine ${RATOOLS_DIR}/rascript-cli.exe -i ${HOME}/${GAME_ID}.rascript -o ${RALIBRETRO_DIR}
 set -o pipefail
-node ${HOME}/autocr-cli-${AUTOCRCLI_VERSION}/index.js --notes ${RA_DATA_DIR}/${GAME_ID}-Notes.json --user ${RA_DATA_DIR}/${GAME_ID}-User.txt --rich ${RA_DATA_DIR}/${GAME_ID}-Rich.txt${REPORT_STR}${SEVERITY_TXT} | tee ${RA_DATA_DIR}/${GAME_ID}-Report.txt
+node ${INSTALLS_DIR}/autocr-cli-${AUTOCRCLI_VERSION}/index.js --notes ${RA_DATA_DIR}/${GAME_ID}-Notes.json --user ${RA_DATA_DIR}/${GAME_ID}-User.txt --rich ${RA_DATA_DIR}/${GAME_ID}-Rich.txt${REPORT_STR}${SEVERITY_TXT} | tee ${RA_DATA_DIR}/${GAME_ID}-Report.txt
 exit $?
